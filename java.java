b5a54c28c83a4c25248548dfc59bfbfc68a3131c
wesley <gameking232nd@gmail.com>
2023-09-15 15:54:02 +0800

    debugging java

--- java.java
+++ java.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class java{
     public static void main(String[]args){
         System.out.println("Hello, World!");
-        Scanner myobj = new Scanner(System.in);
-        int first = myobj.nextInt();
-        int b = first;
+        Scanner input = new Scanner(System.in);
+        String a = input.next();
+        int b = input.nextInt();
         int a1= b*5;
-        System.out.println("hello, World num of 5!"+ b);
-        System.out.println("hello, World!"+a1);
+        System.out.println("hello, World num of 5 !"+ a1);
+        System.out.println("hello, World !"+ a);
     }
 }
