2bf9483226ab0e35bb4b5172b9096581d1ffed84
wesley <gameking232nd@gmail.com>
2023-09-15 14:20:05 +0800

    sample file

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class main
+{
+    public static void main (String args[])
+    {
+        String name, fname, sname;
+        System.out.println("write your first name: ");
+        Scanner myobj = new Scanner(System.in);
+        String first = myobj.nextLine();
+        System.out.println("write your last name: ");
+        Scanner myobj2 = new Scanner (System.in);
+        String last = myobj2.nextLine();
+        fname = first;
+        sname = last;
+        name = fname + " " + sname;
+        System.out.println("Hello "+ name);
+
+
+    }
+}   
